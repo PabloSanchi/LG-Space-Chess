871017ff37c804b9361369bc4a1dde2d54019bbf
Pablo <pablo.sanchi.herrera@gmail.com>
2022-07-14 19:37:48 +0200

    cors .

--- next.config.js
+++ next.config.js
@@ -2,18 +2,20 @@
 
 
 const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [{
-          key: 'Content-Security-Policy',
-          value: 'upgrade-insecure-requests'
-        }],
-      },
-    ]
-  },
-  
+  reactStrictMode: true,  
 }
+
 module.exports = nextConfig
+
+
+// async headers() {
+//   return [
+//     {
+//       source: '/:path*',
+//       headers: [{
+//         key: 'Content-Security-Policy',
+//         value: 'upgrade-insecure-requests'
+//       }],
+//     },
+//   ]
+// },
